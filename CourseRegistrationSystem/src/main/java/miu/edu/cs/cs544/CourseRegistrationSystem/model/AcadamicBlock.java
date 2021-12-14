49a68571d82668be59d21ed8ff200044299bfc3a
yordismo <yordanos.mengestab@gmail.com>
2021-12-14 15:59:26 -0600

    update

--- CourseRegistrationSystem/src/main/java/miu/edu/cs/cs544/CourseRegistrationSystem/model/AcadamicBlock.java
+++ CourseRegistrationSystem/src/main/java/miu/edu/cs/cs544/CourseRegistrationSystem/model/AcadamicBlock.java
@@ -1,12 +1,21 @@
 package miu.edu.cs.cs544.CourseRegistrationSystem.model;
 
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
 
 public class AcadamicBlock {
-    private Integer id;
-    private String code;
-    private String name;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate StartDate;
+
+    private LocalDate EndDate;
+
+
+    private List<CourseOffering> courseOfferings;
+
 
     @Enumerated(EnumType.STRING)
     private Semester semester;
