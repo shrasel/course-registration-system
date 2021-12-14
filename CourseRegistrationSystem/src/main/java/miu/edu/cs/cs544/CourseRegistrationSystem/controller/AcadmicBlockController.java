0ef9900c5ecd849b1a64026d542cd895451ea180
yordismo <yordanos.mengestab@gmail.com>
2021-12-14 15:54:33 -0600

    update

--- CourseRegistrationSystem/src/main/java/miu/edu/cs/cs544/CourseRegistrationSystem/controller/AcadmicBlockController.java
+++ CourseRegistrationSystem/src/main/java/miu/edu/cs/cs544/CourseRegistrationSystem/controller/AcadmicBlockController.java
@@ -1,4 +1,30 @@
 package miu.edu.cs.cs544.CourseRegistrationSystem.controller;
 
+import lombok.Data;
+import miu.edu.cs.cs544.CourseRegistrationSystem.model.CourseOffering;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@Entity
 public class AcadmicBlockController {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate StartDate;
+
+    private LocalDate EndDate;
+
+    private String Semester;
+
+    private List<CourseOffering> courseOfferings;
+
+
+
 }
